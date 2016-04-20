5bcca07d50244939fb79b76fc3aa10014a1da01e
maxi falcone <maxifalcone@Mac-mini-de-maxi.local>
2016-04-20 19:37:32 -0300

    navegacion de paginas

--- wp-content/themes/ciudadfutura/functions.php
+++ wp-content/themes/ciudadfutura/functions.php
@@ -140,4 +140,22 @@ function wpse_91693_render()
 
 }
 
+function pagination_bar() {
+    global $wp_query;
+ 
+    $total_pages = $wp_query->max_num_pages;
+ 
+    if ($total_pages > 1){
+        $current_page = max(1, get_query_var('paged'));
+ 
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => '/page/%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+        ));
+    }
+}
+
+
 ?>
