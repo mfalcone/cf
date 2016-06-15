39ed606f07e6be7d9c009f9258f42619d35c3ea7
maxi falcone <maxifalcone@Mac-mini-de-maxi.local>
2016-06-15 16:11:01 -0300

    facebook

--- wp-content/themes/ciudadfutura/functions.php
+++ wp-content/themes/ciudadfutura/functions.php
@@ -66,7 +66,7 @@ function fb_opengraph() {
  
     if(is_single()) {
         if(has_post_thumbnail($post->ID)) {
-            $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'medium');
+            $img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'large');
         } else {
             $img_src = get_stylesheet_directory_uri() . '/img/ciudad_hacer.jpg';
         }
@@ -83,7 +83,7 @@ function fb_opengraph() {
     <meta property="og:type" content="article"/>
     <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
     <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
-    <meta property="og:image" content="<?php echo $img_src; ?>"/>
+    <meta property="og:image" content="<?php echo $img_src[0]; ?>"/>
  
 <?php
     } else if(is_page()){
